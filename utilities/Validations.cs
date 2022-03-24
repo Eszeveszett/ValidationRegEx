03eae702df6d1e5b29977dedab25b1d1040771a8
Szántó Benjámin <61885011+vellt@users.noreply.github.com>
2022-03-24 18:26:19 +0100

    a shortened validations class

--- utilities/Validations.cs
+++ utilities/Validations.cs
@@ -17,52 +17,30 @@ namespace ValidationsRegEx.utilities
         Email
     }
 
-    public static class RegExPatterns {
-        
-        public static string Int => @"^[0-9]+$";
-        public static string Double => @"^-?(?:0|[1-9][0-9]*)\.?[0-9]+$";
-        public static string Number => @"^[0-9]+([\\.][0-9]+)?$";
-        public static string Phone => @"((?:\+?3|0)6)(?:-|\()?(\d{1,2})(?:-|\))?(\d{3})-?(\d{3,4})";
-        public static string Email => @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
-    }
+    
 
     public static class Validations
     {
+        static class RegExPatterns
+        {
+            public static string Int => @"^[0-9]+$";
+            public static string Double => @"^-?(?:0|[1-9][0-9]*)\.?[0-9]+$";
+            public static string Number => @"^[0-9]+([\\.][0-9]+)?$";
+            public static string Phone => @"((?:\+?3|0)6)(?:-|\()?(\d{1,2})(?:-|\))?(\d{3})-?(\d{3,4})";
+            public static string Email => @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+        }
+
         public static bool IsValid(this string text, For validation)
         {
             return validation switch
             {
-                For.Int => Int(text),
-                For.Double => Double(text),
-                For.Number => Number(text),
-                For.Phone => Phone(text),
-                For.Email => Email(text),
+                For.Int => new Regex(RegExPatterns.Int).IsMatch(text),
+                For.Double => new Regex(RegExPatterns.Double).IsMatch(text),
+                For.Number => new Regex(RegExPatterns.Number).IsMatch(text),
+                For.Phone => new Regex(RegExPatterns.Phone).IsMatch(text),
+                For.Email => new Regex(RegExPatterns.Email).IsMatch(text),
                 _ => false,
             };
-        }
-
-        private static bool Number(string text)
-        {
-            return new Regex(RegExPatterns.Number).IsMatch(text);
-        }
-
-        private static bool Int(string text)
-        {
-            return new Regex(RegExPatterns.Int).IsMatch(text);
-        }
-
-        private static bool Double(string text)
-        {
-            return new Regex(RegExPatterns.Double).IsMatch(text);
-        }
-
-        private static bool Phone(string text)
-        {
-            return new Regex(RegExPatterns.Phone).IsMatch(text);
-        }
-        private static bool Email(string text)
-        {
-            return new Regex(RegExPatterns.Email).IsMatch(text);
         }
     }
 }
