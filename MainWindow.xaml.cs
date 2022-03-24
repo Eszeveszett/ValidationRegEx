dc13c15e4e28fd02860ab05a7b3e2ed9e7df4d41
Miklós Gruber <gyemantlelek@outlook.com>
2022-03-24 13:53:03 +0100

    commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Text.RegularExpressions;   //  A regex névtere
+using ValidationRegEx.utilities;
+
+namespace ValidationRegEx
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+            // "[^0-9]+" : Szám "[^a-z]+" : Az angol ABC kisbetűi "[^A-Z]+" : Az angol ABC nagybetűi "[^A-z]+" : Az angol ABC kis és nagybetűi
+        }
+        private void number_tbx_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            nummber_label.Background = ((TextBox)sender).Text.IsValid(For.Int) ? Brushes.Green : Brushes.Red;
+        }
+
+        private void phone_tbx_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            phone_label.Background = ((TextBox)sender).Text.IsValid(For.Phone) ? Brushes.Green : Brushes.Red;
+        }
+
+        private void email_tbx_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            email_label.Background = ((TextBox)sender).Text.IsValid(For.Email) ? Brushes.Green : Brushes.Red;
+        }
+    }
+}
---
+++ utilities/Validation.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ValidationRegEx.utilities
+{
+    public enum For
+    {
+        Int,
+        Double,
+        Number,
+        Phone,
+        Email
+    }
+
+    public static class RegExPatterns
+    {
+
+        public static string Int => @"^[0-9]+$";
+        public static string Double => @"^-?(?:0|[1-9][0-9]*)\.?[0-9]+$";
+        public static string Number => @"^[0-9]+([\\.][0-9]+)?$";
+        public static string Phone => @"((?:\+?3|0)6)(?:-|\()?(\d{1,2})(?:-|\))?(\d{3})-?(\d{3,4})";
+        public static string Email => @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+    }
+
+    public static class Validations
+    {
+        public static bool IsValid(this string text, For validation)
+        {
+            return validation switch
+            {
+                For.Int => Int(text),
+                For.Double => Double(text),
+                For.Number => Number(text),
+                For.Phone => Phone(text),
+                For.Email => Email(text),
+                _ => false,
+            };
+        }
+
+        private static bool Number(string text)
+        {
+            return new Regex(RegExPatterns.Number).IsMatch(text);
+        }
+
+        private static bool Int(string text)
+        {
+            return new Regex(RegExPatterns.Int).IsMatch(text);
+        }
+
+        private static bool Double(string text)
+        {
+            return new Regex(RegExPatterns.Double).IsMatch(text);
+        }
+
+        private static bool Phone(string text)
+        {
+            return new Regex(RegExPatterns.Phone).IsMatch(text);
+        }
+        private static bool Email(string text)
+        {
+            return new Regex(RegExPatterns.Email).IsMatch(text);
+        }
+    }
+}
